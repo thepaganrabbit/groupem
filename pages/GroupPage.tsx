9fb58bc902989725c3582f315f82718e42a0328f
thepaganrabbit <gonzalesgustavoc@gmail.com>
2024-02-13 10:37:19 -0700

    feat: changed buttons to icons. Closes #16

--- pages/GroupPage.tsx
+++ pages/GroupPage.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { View, Text, ToastAndroid, ActivityIndicator, StyleSheet, ScrollView, Button } from 'react-native';
+import { View, Text, ToastAndroid, ActivityIndicator, StyleSheet, ScrollView, Button, Pressable } from 'react-native';
 import { Dispatch } from '../store';
 import { useDispatch } from 'react-redux';
 import { Contact } from 'react-native-contacts';
 import { WEBCOLOURS, randomNumberGenerator } from '../Utils';
 import { Table, Row, Rows, TableWrapper } from 'react-native-table-component';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const GrouPage = ({ route, navigation }: any): React.JSX.Element => {
     const { groupID } = route.params;
     const dispatch: Dispatch = useDispatch();
     let navIfFailed: NodeJS.Timeout;
     const tableData: {heads: any[], rows: any[]} = {
-        heads: ['name', 'phone', 'email'],
+        heads: ['name', 'phone', 'email', 'call', 'text', 'delete'],
         rows: []
     }
     const group = React.useMemo(() => {
@@ -27,10 +29,13 @@ const GrouPage = ({ route, navigation }: any): React.JSX.Element => {
             }, 15000)
         }
        (found_group.contacts.forEach((fGroup: Contact) => {
-            const rowArr: string[] = [
+            const rowArr: any[] = [
                 fGroup.displayName,
                 fGroup.phoneNumbers.length > 0 ? fGroup.phoneNumbers.filter((number) => number.label === 'mobile')[0].number : 'No Number Provided',
-                fGroup.emailAddresses.length > 0 ? fGroup.emailAddresses.filter((email) => email.label === 'home')[0].email : 'No Email Provided'
+                fGroup.emailAddresses.length > 0 ? fGroup.emailAddresses.filter((email) => email.label === 'home')[0].email : 'No Email Provided',
+                <Pressable><Icon name="phone" size={30} style={{textAlign: 'center'}}/></Pressable>,
+                <Pressable><MaterialCommunityIcon style={{textAlign: 'center'}} name="message" size={30} /></Pressable>,
+                <Pressable><Icon name="trash" size={30} color={WEBCOLOURS[15]} style={{textAlign: 'center'}} /></Pressable>
             ];
            tableData.rows.push(rowArr);
         }));
@@ -48,9 +53,9 @@ const GrouPage = ({ route, navigation }: any): React.JSX.Element => {
                 <Text style={styles.bolded}>Users in this group:</Text>
                 <ScrollView>
                 <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-                    <Row data={tableData.heads} style={styles.head} />   
+                    <Row data={tableData.heads} style={styles.head}  textStyle={{textAlign: 'center'}} />   
                     <TableWrapper>
-                    <Rows data={tableData.rows} style={{padding: 3}} />
+                    <Rows data={tableData.rows}  textStyle={{textAlign: 'center'}}/>
                     </TableWrapper>
                 </Table>
                 <View style={{marginTop: 22, flex: 1, flexDirection: 'column', width: '80%', alignSelf: 'center'}}>
@@ -87,7 +92,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     fontWeight: '900',
    },
-   head: { height: 40, backgroundColor: '#f1f8ff' },
+   head: { height: 40, backgroundColor: '#f1f8ff'},
    box: {
     borderWidth: 1,
     padding: 4,
